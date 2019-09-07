1ddd6623c7f6ce09f57689fa137f9f75b413fbcd
unknown <wonrok97@naver.com>
2019-09-07 10:53:44 +0900

    in과 .count('~')

--- Simple_369.py
+++ Simple_369.py
@@ -1,7 +1,7 @@
 N = int(input())
 arr = [str(i) for i in range (0,N+1)]
 for i in range (1, len(arr)):
-    if arr[i].count('3') > 0 or arr[i].count('6') > 0 or arr[i].count('9') > 0:
+    if arr[i].count('3') > 0 or arr[i].count('6') > 0 or arr[i].count('9') > 0:             # in을 사용 할 시 더편함 '3' in i ->i에 3이 있는지
         print('-'*arr[i].count('3')+'-'*arr[i].count('6')+'-'*arr[i].count('9'),end =' ')
     else:
         print(arr[i], end=' ')
