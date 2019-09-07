e17d8df8a7f078a5e4632c54c915c931500da249
unknown <wonrok97@naver.com>
2019-09-07 10:40:37 +0900

    동일객체_취급

--- Pascal.py
+++ Pascal.py
@@ -7,8 +7,8 @@ for i in range(1, t + 1):
     N = int(input())
     print("#{}".format(i))
     for j in range(1, N + 1):
-        pre_arr = arr.copy()
-        del arr[0:]
+        pre_arr = arr.copy()    #arr.copy()가 아님 arr쓸 pre_arr과 arr이 같은주소를 가르킴
+        del arr[0:]             #so, 이부분에서 arr를 비울 시 pre_arr도 비워짐- 그래서else부분에서 인덱스에러뜸
         for z in range(0, j):
             if z == j-1:
                 arr += [1]
