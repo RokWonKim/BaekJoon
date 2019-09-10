603507ff1bc818e40586c52ff236dfd836fdcd23
rokwon <rokwon@localhost.localdomain>
2019-09-10 21:42:04 +0900

    .count()

--- Simplecompress.py
+++ Simplecompress.py
@@ -1,3 +1,4 @@
+#간단한 압축 풀기 문제 1946
 t = int(input())
 for i in range(1, t + 1):
     N = int(input())
@@ -8,8 +9,8 @@ for i in range(1, t + 1):
         Ciki = input()
         Ciki = Ciki.split(' ')
         arr += [Ciki]
-    for j in range(0, N):       #알파뱃*숫자를 하나의 문장으로 만듬
-        sentance += arr[j][0] * int(arr[j][1])
+    for j in range(0, N):       #알파뱃*숫자를 하나의 문장으로 만들어서 계속 그 뒤에 붙임
+        sentance += arr[j][0] * int(arr[j][1]) #[j][0]에는 알파뱃이 [j][1]에는 몇번 반복해야할지가 들어있음
     print(sentance[0], end='')
     for j in range(1, len(sentance)):   #10개씩 출력
         if j % 10 == 0:
