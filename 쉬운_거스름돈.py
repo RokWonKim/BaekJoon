5cb9f2a9b99cc0d1180f9ccb901f235b8f009757
RokWonKim <wonrok97@naver.com>
2019-09-13 21:52:05 +0900

    len()활용

--- 쉬운_거스름돈.py
+++ 쉬운_거스름돈.py
@@ -4,7 +4,7 @@ for i in range(1,t+1):
     money = int(input())
     print("#%d" % i)
     arr_money = [50000,10000,5000,1000,500,100,50,10]
-    for j in range(0,8):
+    for j in range(0,len(arr_money)):
         print(money//arr_money[j], end = ' ')
         money = money%arr_money[j]
     print()
