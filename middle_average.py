7249d1c2b3a32b43735b05d88ec9547320edd936
RokWonKim <wonrok97@naver.com>
2019-09-19 23:57:40 +0900

    sum()활용

--- middle_average.py
+++ middle_average.py
@@ -1,9 +1,7 @@
 t = int(input())
 for i in range(1,t+1):
     print("#%d" % i,end = ' ')
-    nums = input()
-    nums = nums.split()
-    nums = [int(x) for x in nums]		
+    nums =list(map(int,input().split()))
     nums.sort()								#정렬
     del nums[0]								#가장 적은 수 삭제
     del nums[len(nums)-1]			#가장 큰 수 삭제 
