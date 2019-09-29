ce35fef5bb8688f64aa310dbcfe1ba104c28df7d
RokWonKim <wonrok97@naver.com>
2019-09-30 00:12:52 +0900

    DP사용

---
+++ 1로_만들기_1463.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+    vector<int> v(N+1);
+    
+    if (N>=2) v[2] = 1;
+    
+    int count = 0;
+    vector<int> min_val;
+    for(int i = 3; i <= N; i++) {
+        count = 0;
+        if(i%2==0) min_val.push_back(v[i/2]);
+        if(i%3==0) min_val.push_back(v[i/3]);
+        min_val.push_back(v[i-1]);
+        v[i] = *min_element(min_val.begin(), min_val.end())+1;
+	min_val.clear();
+    }
+    cout << v[N];
+    return 0;
+}
