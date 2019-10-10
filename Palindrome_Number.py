a07ff55332fbef9a60267d7cf715694d0047ce57
RokwonK <52196792+RokwonK@users.noreply.github.com>
2019-10-11 03:17:07 +0900

    Delete Palindrome_Number.py

--- Palindrome_Number.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        if x >= 0 and str(x) == str(x)[::-1]: return True
-        else: return False
-
-s = Solution()
-num = s.isPalindrome(121)
-print(num)
